7dbc30ebc6fadfd1ac9c9bbfd445145ecd613100
Marek Kochanowicz <sirherrbatka@gmail.com>
2021-04-05 19:44:08 +0200

    Added translations of the conditions.

--- src/conditions.lisp
+++ src/conditions.lisp
@@ -3,8 +3,10 @@
 
 (define-condition missing-required-slot (cl-ds:initialization-error)
   ((%keyarg :initarg :keyarg)
-   (%slot-name :initarg :slot-name)
-   (%object :initarg :object)))
+   (%slot-name :initarg :slot-name
+               :reader slot-name)
+   (%object :initarg :object
+            :reader layer)))
 
 
 (defun missing-required-slot (slot-name keyarg object)
@@ -14,3 +16,25 @@
          :slot-name slot-name
          :format-control "No value ~a for initialization of the ~a."
          :format-arguments (list keyarg object)))
+
+
+(defun read-new-value ()
+  (format t "Enter a new value: ")
+  (multiple-value-list (eval (read))))
+
+
+(defmacro with-bind-slot-restart (&body body)
+  (with-gensyms (!condition !block)
+    `(tagbody ,!block
+        (let ((,!condition nil))
+          (restart-case (handler-case (progn ,@body)
+                          (condition (e)
+                            (setf ,!condition e)
+                            (error e)))
+            (bind-slot (new-value)
+              :interactive read-new-value
+              :report "Bind slot of the object."
+              (setf (slot-value (layer ,!condition)
+                                (slot-name ,!condition))
+                    new-value)
+              (go ,!block)))))))
